a9a8892bc04b5d7fe98cb81d23e60e2e917817d1
Oleksandr_Moroz <110786733+Kontrabas888@users.noreply.github.com>
2023-03-20 13:45:19 +0200

    hw-1-react-cor3

--- src/components/statistics/Statistics.jsx
+++ src/components/statistics/Statistics.jsx
@@ -5,19 +5,22 @@ import cssStatist from "../statistics/Statistics.module.css";
 export const Statistics = ({ statistics }) => {
   return (
     <section className={cssStatist.statistic}>
-      <ul className={cssStatist["stats-list"]}>
-        {statistics.map((item, index) => (
-          <li
-            key={item.id}
-            className={clsx(cssStatist.item, {
-              [cssStatist["item--special"]]: index % 5 === 0,
-            })}
-          >
-            <span className={cssStatist.label}>{item.label}</span>
-            <span className={cssStatist.percentage}>{item.percentage}%</span>
-          </li>
-        ))}
-      </ul>
+
+      <h2 className={cssStatist.title}>Upload stats</h2>
+
+        <ul className={cssStatist["stats-list"]}>
+          {statistics.map((item, index) => (
+            <li
+              key={item.id}
+              className={clsx(cssStatist.item, {
+                [cssStatist["item--special"]]: index % 5 === 0,
+              })}
+            >
+              <span className={cssStatist.label}>{item.label}</span>
+              <span className={cssStatist.percentage}>{item.percentage}%</span>
+            </li>
+          ))}
+        </ul>
     </section>
   );
 };
